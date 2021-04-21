6ca59def93889d671518be4b0f082402f53fff17
prodhc <zionywood@gmail.com>
2021-04-21 14:20:51 -0700

    first, form created

---
+++ appthree/src/Form.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export function Form() {
+    return (
+        <div>
+            <form>
+                <label>User
+                    <input onChange={} value={} name='user' type="text" />
+                </label>
+
+                <label>
+                <input onChange={} value={} name='user' type="text" />
+                </label>
+
+                <label>
+                <input onChange={} value={} name='user' type="text" />
+                </label>
+
+                <label>
+                    <input/>
+                </label>
+                <button></button>
+            </form>
+        </div>
+    )   
+}
